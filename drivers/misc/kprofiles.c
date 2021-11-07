bb7e6997977eff84f41668e20367876b8208c829
Dakkshesh <dakkshesh5@gmail.com>
2021-11-07 15:57:48 +0530

    kprofiles: Introduce 2 new API for changing mode on custom events
    This commit introduces two new APIs for other drivers
    to request a profile mode change. So this new APIs gives us the flexibility to
    change profile on any custom event.
    
    kprofiles_set_mode_rollback() : This function takes 2 parameters as input, 1st is the profile mode that it will switch to
    and other for the amount of time (in milliseconds) to stay at that mode before switching back to the previously active mode.
    
    kprofiles_set_mode() : This function is rather simple compared to the previous function, it only takes 1 input parameter,
    which is the mode to switch the profile to, the mode will not change until this function is manually used again to change the mode.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -6,31 +6,135 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kprofiles.h>
+#include <linux/delay.h>
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+#include <linux/msm_drm_notify.h>
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#include <linux/fb.h>
+#endif
 
-unsigned int mode = 0;
+static bool screen_on = true;
+static unsigned int mode = 0;
+static unsigned int set_mode;
 module_param(mode, uint, 0664);
 
-
-unsigned int active_mode(void) {
-	if (mode == 1) {
-		return 1;
-	}
-
-	if (mode == 2) {
-		return 2;
-	}
-
-	if (mode == 3) {
-		return 3;
-	}
-
-	else {
-		pr_info("Invalid value passed, falling back to level 0\n");
-		return 0;
-	}
+void kprofiles_set_mode_rollback(unsigned int level, unsigned int duration_ms)
+{
+	if (!level || !duration_ms)
+		return;
+	set_mode = mode;
+	mode = level;
+	msleep(duration_ms);
+	mode = set_mode;
 }
 
+void kprofiles_set_mode(unsigned int level)
+{
+	if (!level)
+		return;
+	mode = level;
+}
+
+#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
+static int common_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+	struct msm_drm_notifier *evdata = data;
+	int *blank;
+
+	if (event != MSM_DRM_EVENT_BLANK)
+		goto out;
+
+	if (!evdata || !evdata->data || evdata->id != MSM_DRM_PRIMARY_DISPLAY)
+		goto out;
+
+	blank = evdata->data;
+	switch (*blank) {
+	case MSM_DRM_BLANK_POWERDOWN:
+		if (!screen_on)
+			break;
+		screen_on = false;
+		mode = 1;
+		break;
+	case MSM_DRM_BLANK_UNBLANK:
+		if (screen_on)
+			break;
+		screen_on = true;
+		mode = set_mode;
+		break;
+	}
+
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+	struct fb_event *evdata = data;
+	int *blank;
+
+	if (event != FB_EVENT_BLANK)
+		goto out;
+
+	blank = evdata->data;
+	switch (*blank) {
+	case FB_BLANK_POWERDOWN:
+		if (!screen_on)
+			break;
+		screen_on = false;
+		mode = 1;
+		break;
+	case FB_BLANK_UNBLANK:
+		if (screen_on)
+			break;
+		screen_on = true;
+		mode = set_mode;
+		break;
+	}
+#endif
+
+out:
+	return NOTIFY_OK;
+}
+#endif
+
+inline unsigned int active_mode(void)
+{
+  switch(mode)
+    {
+    case 0:
+      return 0;
+      break;
+    case 1:
+      return 1;
+      break;
+    case 2:
+      return 2;
+      break;
+    case 3:
+      return 3;
+      break;
+    default:
+      pr_info("Invalid value passed, falling back to level 0\n");
+      return 0;
+    }
+}
+
+#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
+static struct notifier_block common_notifier_block = {
+	.notifier_call = common_notifier_callback,
+};
+
+static int  __init kprofiles_notifier_init(void)
+{
+
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+	msm_drm_register_client(&common_notifier_block);
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+	fb_register_client(&common_notifier_block);
+#endif
+	return 0;
+}
+
+late_initcall(kprofiles_notifier_init);
+#endif
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dakkshesh");
 MODULE_DESCRIPTION("KernelSpace Profiles");
-MODULE_VERSION("1.0.0"); 
+MODULE_VERSION("2.0.0");
